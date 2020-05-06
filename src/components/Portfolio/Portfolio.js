0cbfd4a8591076b6815a4f19c42dc188848cb961
udosinachi <udosipat@gmail.com>
2020-05-06 16:03:43 +0100

    Portfolio

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -7,77 +7,77 @@ class Portfolio extends React.Component{
             <Div>
                 <div>
                 <span>
-               <span className='span1'>
+               <span className='span1'> 
                     <img src='/Asset/24img/1.jpg' alt='1'/>
                 </span>
                 <span className='span2'>
-                    <img src='/Asset/24img/2.jpg' alt='1' />
+                    <img src='/Asset/24img/2.jpg' alt='2' />
                 </span>
                 <span className='span3'>
-                    <img src='/Asset/24img/3.jpg' alt='1' />
+                    <img src='/Asset/24img/3.jpg' alt='3' />
                 </span>
                 <span className='span4'>
-                    <img src='/Asset/24img/4.jpg' alt='1' />
+                    <img src='/Asset/24img/4.jpg' alt='4' />
                 </span>
                 <span className='span5'>
-                    <img src='/Asset/24img/5.jpg' alt='1' />
+                    <img src='/Asset/24img/5.jpg' alt='5' />
                 </span>
                 <span className='span6'>
-                    <img src='/Asset/24img/a.jpg' alt='1' />
+                    <img src='/Asset/24img/a.jpg' alt='6' />
                 </span>
                 <span className='span7'>
-                    <img src='/Asset/24img/b.jpg' alt='1' />
+                    <img src='/Asset/24img/b.jpg' alt='7' />
                 </span>
                 <span className='span8'>
-                    <img src='/Asset/24img/c.jpg' alt='1' />
+                    <img src='/Asset/24img/c.jpg' alt='8' />
                 </span>
                 <span className='span9'>
-                    <img src='/Asset/24img/d.jpg' alt='1' />
+                    <img src='/Asset/24img/d.jpg' alt='9' />
                 </span>
                 <span className='span10'>
-                    <img src='/Asset/24img/e.jpg' alt='1' />
+                    <img src='/Asset/24img/e.jpg' alt='10' />
                 </span>
                 <span className='span11'>
-                    <img src='/Asset/24img/f.jpg' alt='1' />
+                    <img src='/Asset/24img/f.jpg' alt='11' />
                 </span>
                 <span className='span12'>
-                    <img src='/Asset/24img/g.jpg' alt='1' />
+                    <img src='/Asset/24img/g.jpg' alt='12' />
                 </span>
                 <span className='span13'>
-                    <img src='/Asset/24img/h.jpg' alt='1' />
+                    <img src='/Asset/24img/h.jpg' alt='13' />
                 </span>
                 <span className='span14'>
-                    <img src='/Asset/24img/i.jpeg' alt='1' />
+                    <img src='/Asset/24img/i.jpeg' alt='14' />
                 </span>
                 <span className='span15'>
-                    <img src='/Asset/24img/images (3).jpeg' alt='1' />
+                    <img src='/Asset/24img/images (3).jpeg' alt='15' />
                 </span>
                 <span className='span16'>
-                    <img src='/Asset/24img/j.jpeg' alt='1' />
+                    <img src='/Asset/24img/j.jpeg' alt='16' />
                 </span>
                 <span className='span17'>
-                    <img src='/Asset/24img/k.jpg' alt='1' />
+                    <img src='/Asset/24img/k.jpg' alt='17' />
                 </span>
                 <span className='span18'>
-                    <img src='/Asset/24img/l.jpg' alt='1' />
+                    <img src='/Asset/24img/l.jpg' alt='18' />
                 </span>
                 <span className='span19'>
-                    <img src='/Asset/24img/m.jpg' alt='1' />
+                    <img src='/Asset/24img/m.jpg' alt='19' />
                 </span>
                 <span className='span20'>
-                    <img src='/Asset/24img/n.jpg' alt='1' />
+                    <img src='/Asset/24img/n.jpg' alt='20' />
                 </span>
                 <span className='span21'>
-                    <img src='/Asset/24img/Portfolio1.PNG' alt='1' />
+                    <img src='/Asset/24img/Portfolio1.PNG' alt='21' />
                 </span>
                 <span className='span22'>
-                    <img src='/Asset/24img/Portfolio2.PNG' alt='1' />
+                    <img src='/Asset/24img/Portfolio2.PNG' alt='22' />
                 </span>
                 <span className='span23'>
-                    <img src='/Asset/24img/Shoppingcart.PNG' alt='1' />
+                    <img src='/Asset/24img/Shoppingcart.PNG' alt='23' />
                 </span>
                 <span className='span24'>
-                    <img src='/Asset/24img/udohub.PNG' alt='1' />
+                    <img src='/Asset/24img/udohub.PNG' alt='24' />
                 </span>
                </span>
                 </div>
