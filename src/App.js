d9a0dde13ddc19b08894caee10ed4621652ed9ec
udosinachi <udosipat@gmail.com>
2020-06-09 16:53:53 +0100

    Portfolio

--- src/App.js
+++ src/App.js
@@ -1,32 +1,13 @@
 import React from 'react'
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
-import './App.css'
 
-import Home from './components/Home/Home'
-import About from './components/About/About'
-import Portfolio from './components/Portfolio/Portfolio'
-import Contact from './components/Contact/Contact'
-import Error from './components/Error/Error'
-import Navbar from './components/Navbar/Navbar'
-import Footer from './components/Footer/Footer'
-
-class App extends React.Component{
-  render(){
-    return (
-      <BrowserRouter>
-        <Navbar />
-          <Switch>
-            <Route path='/' component={Home} exact />
-            <Route path='/about' component={About} />
-            <Route path='/portfolio' component={Portfolio} />
-            <Route path='/contact' component={Contact} />
-            <Route component={Error} />
-          </Switch>
-          <Route path='/' component={Footer} />
-      </BrowserRouter>
-      
-    )
-  }
+function App(){
+  return(
+    <div>
+      <h1>
+        ghfjk
+      </h1>
+    </div>
+  )
 }
 
-export default App;
+export default App
--- src/components/Portfolio/PortfolioStyle.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components'
-
-export const Div = styled.div`
-background: grey;
-overflow: hidden;
-height: 100vh;
-`
