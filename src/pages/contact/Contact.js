3ca328f2c6ced685656ae6a42baae50cb4f34fd0
udosinachi <udosipat@gmail.com>
2020-08-12 18:36:00 +0100

    Almost done with contact style

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -18,8 +18,18 @@ class Contact extends React.Component{
                             <input type='text' placeholder='Name' /><br/>
                             <input type='email' placeholder='Email Address' /><br/>
                             <input type='text' placeholder='Subject' /><br/>
-                            <textarea placeholder='Hello there' />
+                            <textarea placeholder='Hello there' /><br/>
+                            <button>Let's Talk Soon</button>
                         </form>
+                        <div className='social'>
+                            <p>Ready? If you're ready to talk then I'm here to listen. Send me a message and we'll start a conversation towards your next design. I look forward to building new sites or making invitations for that special event. I also enjoy meeting and collaborating with other designers. If you have a UI concept you'd like to see become a reality but lack the coding portion let's see what we can do. Media inquiries? Sure</p>
+                            <div className='icons'>
+                                <a href='https://www.google.com' target='_blank'><img src='/asset/svg/1.svg' alt='icon' className='1' /></a>
+                                <a href='https://www.google.com' target='_blank'><img src='/asset/svg/3.svg' alt='icon' className='2' /></a>
+                                <a href='https://www.google.com' target='_blank'><img src='/asset/svg/4.svg' alt='icon' className='3' /></a>
+                                <a href='https://www.google.com' target='_blank'><img src='/asset/svg/8.svg' alt='icon' className='4' /></a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </Div>
--- src/pages/contact/Contactstyle.js
+++ src/pages/contact/Contactstyle.js
@@ -11,5 +11,44 @@ overflow: hidden;
             padding: 0;
         }
     }
+    .bodyform{
+        display: flex;
+        justify-content: space-evenly;
+        flex-wrap: wrap;
+        margin: 50px 100px;
+        form{
+            input, textarea, button{
+                width: 500px;
+                padding: 20px 5px;
+                margin: 5px;
+            }
+            textarea{
+                height: 100px;
+            }
+            button{
+                width: 510px;
+                color: white;
+                background: black;
+                outline: none;
+                border: none;
+            }
+        }
+        .social{
+            width: 500px;
+            margin-top: 60px;
+            p{
+                font-size: 20px;
+            }
+            .icons{
+                width: 200px;
+                display: flex;
+                justify-content: space-evenly;
+                img{
+                    width: 30px;
+                    cursor: pointer;
+                }
+            }
+        }
+    }
 }
 `
