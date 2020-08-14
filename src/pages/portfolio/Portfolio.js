d6ba3b6167f639f5ce0c85ab4ccd39cf0d48ad5c
udosinachi <udosipat@gmail.com>
2020-08-14 20:13:31 +0100

    Portfolio boiler plates

---
+++ src/pages/portfolio/Portfolio.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Div } from './Portfoliostyle'
+
+function Porfolio (){
+    return(
+        <Div>
+            
+        </Div>
+    )
+}
+
+export default Portfolio
