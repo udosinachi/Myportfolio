b3bc19d55255e49d765dc04507e23876609233a1
udosinachi <udosipat@gmail.com>
2020-08-14 20:23:44 +0100

    Added nav bar

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './pages/home/Home'
 import About from './pages/about/About'
 import Contact from './pages/contact/Contact'
 import Error from './Error'
+import Portfolio from './pages/portfolio/Portfolio'
 
 
 function App(){
@@ -14,6 +15,7 @@ function App(){
         <Route exact path='/' component={Home} />
         <Route exact path='/about' component={About} />
         <Route exact path='/contact' component={Contact} />
+        <Route exact path='/portfolio' component={Portfolio} />
         <Route component={Error} />
       </Switch>
     </BrowserRouter>
--- src/pages/portfolio/Portfolio.js
+++ src/pages/portfolio/Portfolio.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import {Div } from './Portfoliostyle'
+import Nav from '../../components/navbar/Nav'
 
-function Porfolio (){
+function Portfolio (){
     return(
         <Div>
-            
+            <Nav navcolor={'silver'} textcolor={'black'} hovercolor={'black'} />
         </Div>
     )
 }
