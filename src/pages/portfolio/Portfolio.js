66c7a94f2e1b5826d230fe10bf49827333a4e2a6
udosinachi <udosipat@gmail.com>
2020-08-14 22:35:49 +0100

    Portfolio design looking good

--- src/pages/portfolio/Portfolio.js
+++ src/pages/portfolio/Portfolio.js
@@ -2,10 +2,19 @@ import React from 'react'
 import {Div } from './Portfoliostyle'
 import Nav from '../../components/navbar/Nav'
 
-function Portfolio (){
+function Portfolio (props){
     return(
         <Div>
             <Nav navcolor={'silver'} textcolor={'black'} hovercolor={'black'} />
+            <div className='layout' style={{marginTop: '130px'}} >
+                <div>
+                    <img src='/asset/img/e.jpg' /><br/>
+                    <div className='inside'>
+                        <h3>My First Project</h3><br/>
+                        <button>Explore</button>
+                    </div>
+                </div>
+            </div>
         </Div>
     )
 }
--- src/pages/portfolio/Portfoliostyle.js
+++ src/pages/portfolio/Portfoliostyle.js
@@ -1,5 +1,30 @@
 import styled from 'styled-components'
 
 export const Div = styled.div`
-
+overflow: hidden;
+.layout{
+    width: 400px;
+    img{
+        width: 400px;
+        position: fixed;
+    }
+    .inside{
+        display: flex;
+        flex-direction: column;
+        margin-top: 55px;
+        h3{
+        z-index: 200;
+        color: white;
+        margin: auto;
+        }
+        button{
+            z-index: 200;
+            width: 150px;
+            padding: 10px 10px;
+            margin: auto;
+            cursor: pointer;
+            background: black;
+        }
+    }
+}
 `
