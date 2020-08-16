9d0d03469e608f719f38bdda6680042d79af45b7
udosinachi <udosipat@gmail.com>
2020-08-16 14:07:59 +0100

    commit

--- src/pages/portfolio/Portfolio.js
+++ src/pages/portfolio/Portfolio.js
@@ -4,13 +4,12 @@ import Nav from '../../components/navbar/Nav'
 
 function Portfolio(props){
     return(
-        <Div>
+        <Div img={props.img}>
             <div>
                 <Nav navcolor={'silver'} textcolor={'black'} hovercolor={'black'} />
             </div>
             <div className='layout' style={{marginTop: '130px'}} >
-                <div>
-                    <img src={props.img} /><br/>
+                <div className='background' >
                     <div className='inside'>
                         <h3>{props.text}</h3><br/>
                         <button>{props.button}</button>
--- src/pages/portfolio/Portfoliostyle.js
+++ src/pages/portfolio/Portfoliostyle.js
@@ -3,37 +3,47 @@ import styled from 'styled-components'
 export const Div = styled.div`
 overflow: hidden;
 .layout{
-    width: 400px;
-    img{
+    width: 500px;
+    @media (max-width: 520px){
         width: 400px;
-        :hover{
-            opacity: 0.9;
-            transition: 0.5s ease;
-        }
     }
-    .inside{
-        display: flex;
-        flex-direction: column;
-        h3{
-        z-index: 100;
-        color: black;
-        margin: auto;
+    @media (max-width: 350px){
+        width: 350px;
+    }
+    .background{
+        height: 300px;
+        background-image: linear-gradient(rgba(8, 1, 43, 0.63), rgba(8, 1, 43, 0.63)), url(${props => props.img});
+        background-repeat: no-repeat;
+        background-position: center;
+        background-size: cover;
+        box-sizing: border-box;
+        @media (max-width: 520px){
+            height: 250px;
         }
-        button{
-            z-index: 100;
-            width: 150px;
-            padding: 10px 10px;
-            margin: auto;
-            cursor: pointer;
-            background: black;
+        .inside{
+            display: flex;
+            flex-direction: column;
+            position: relative;
+            top: 90px;
+            h3{
             color: white;
-            border: none;
-            outline: none;
-            
-            :hover{
-                color: black;
-                background: white;
-                transition: 0.9s ease;
+            margin: auto;
+            }
+            button{
+                width: 150px;
+                padding: 10px 10px;
+                margin: auto;
+                cursor: pointer;
+                background: black;
+                color: white;
+                border: none;
+                outline: none;
+                
+                :hover{
+                    color: black;
+                    background: white;
+                    transition: 0.9s ease;
+                }
             }
         }
     }
@@ -41,10 +51,11 @@ overflow: hidden;
 `
 
 export const Flex = styled.div`
-
+overflow: hidden;
 .div{
     display: flex;
     justify-content: space-evenly;
     flex-wrap: wrap;
+    border: 5px solid yellow;
 }
 `
